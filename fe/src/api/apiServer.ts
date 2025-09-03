97e4a3db10b0e534b7439517f53fa314e126b846
19010853 <khoinguyenminhk37@gmail.com>
2025-09-03 16:17:37 +0700

    Rewrite the vercel on client api url

--- fe/src/api/apiServer.ts
+++ fe/src/api/apiServer.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiServer = axios.create({
-    baseURL: 'https://video-online-beta.vercel.app/',
+    baseURL: 'https://video-online-beta.vercel.app',
     withCredentials: true,
 });
 
